7d8a214f48e874d75f7f9c52f7c73094f02bef3e
unique <0xun1que@0x.studio>
2023-11-16 16:36:33 +0700

    Update api.py

--- modules/api/api.py
+++ modules/api/api.py
@@ -843,17 +843,17 @@ class Api:
 
 
         '''   2.2 Train model and then store on S3'''
-        trainer = LoraModelTrainer()
-        x = threading.Thread(target=trainer.train(), args=(req.ref_id, req.model_name, tmp_dir,))
-        x.start()
+        # trainer = LoraModelTrainer()
+        # x = threading.Thread(target=trainer.train(), args=(req.ref_id, req.model_name, tmp_dir,))
+        # x.start()
 
         '''   Single thread processing '''
-        # trainer = LoraModelTrainer()
-        # model_file = trainer.train(
-        #     ref_id=req.ref_id,
-        #     model_name=req.model_name,
-        #     dataset_dir=tmp_dir
-        # )
+        trainer = LoraModelTrainer()
+        model_file = trainer.train(
+            ref_id=req.ref_id,
+            model_name=req.model_name,
+            dataset_dir=tmp_dir
+        )
 
         return models.LoraModelTrainingResponse(
             status="OK",
